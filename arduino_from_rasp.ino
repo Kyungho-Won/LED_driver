ce3c8c5ce0adc9cbe7497bcbde91078e6bcd2579
Kyungho Won <kyungho.won@inu.ac.kr>
2025-07-15 15:58:14 +0900

    fix pin names

--- arduino_from_rasp.ino
+++ arduino_from_rasp.ino
@@ -1,12 +1,12 @@
-const int pin = LED_BUILTIN; // LED_BUILTIN 또는 원하는 핀 번호로 변경
+const int outPin = 8; // LED_BUILTIN 또는 원하는 핀 번호로 변경
 
 unsigned long pulse_start_time = 0;
 bool pulse_active = false;
 unsigned long pulse_duration_us = 0;
 
 void setup() {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, LOW);
   Serial.begin(9600);
   while (!Serial) {
     ; // 시리얼 연결 대기 (필요 시)
@@ -25,21 +25,21 @@ void loop() {
         int duration_ms = input.substring(durationStart, durationEnd).toInt();
         pulse_duration_us = (unsigned long)duration_ms * 1000;
         pulse_start_time = micros();
-        digitalWrite(pin, HIGH);
+        digitalWrite(outPin, HIGH);
         pulse_active = true;
       }
     } else if (input == "on") {
-      digitalWrite(pin, HIGH);
+      digitalWrite(outPin, HIGH);
       pulse_active = false; // 수동 제어 상태로 전환
     } else if (input == "off") {
-      digitalWrite(pin, LOW);
+      digitalWrite(outPin, LOW);
       pulse_active = false;
     }
   }
 
   // 고정밀 타이머 제어
   if (pulse_active && (micros() - pulse_start_time >= pulse_duration_us)) {
-    digitalWrite(pin, LOW);
+    digitalWrite(outPin, LOW);
     pulse_active = false;
   }
 }
